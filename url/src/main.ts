e2d232af379efee2e3aea9c86d26d2367ed0e894
unknown <gianine1508@gmail.com>
2025-06-22 15:54:00 -0300

    feat(empresa,usuario,url): Inclusao do swagger para documentar os endpoints

--- url/src/main.ts
+++ url/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Servico de Empresa')
+    .setDescription('Api focada em lidar com funcionalidades voltadas a Varias empresas')
+    .setVersion('1.0')
+    .addTag('empresa')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
